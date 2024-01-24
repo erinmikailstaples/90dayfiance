97bbf563ab9a1b090826fd507cb10849a98ed2fa
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2024-01-24 18:03:39 -0500

    dskfasudkfhask l

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,21 +6,44 @@ import altair as alt
 import numpy as np
 import pandas as pd 
 
-# load environment variables + validate SDK Key
+# st_supabase_client = st.connection(
+#     name="90DAYDATABASE",
+#     type=SupabaseConnection,
+#     ttl=None,
+#     url="SUPABASE_URL",
+#     key="SUPABASE_KEY",
+# )
+
+# st_supabase_client.query("*", table="90dayfiance", ttl=0).execute()
+
+
+
+
+# # load environment variables + validate SDK Key
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-#query to get the dang data
-response = supabase.table('90dayfiance').select('partnera').execute()
+data = supabase.table("90dayfiance").select("*").execute()
 
-data = supabase.table("90dayfiance").select("*").execute
 
-df = pd.DataFrame(data.data)
+# # #query to get the dang data
+def read_data():
+    return supabase.from_("90dayfiance").select("*").execute()
+    df = pd.DataFrame(response.data)
+    return df
+
+df = pd.DataFrame(read_data())
+st.dataframe(df)
+
+print(df)
+
+data = supabase.table("90dayfiance").select("*").execute()
+
+df = pd.DataFrame(data)
 
 chart = alt.Chart(df).mark_bar().encode(
     x='partnera',
     y='partnerb'
 )
-
-st.altair_chart(chart, use_container_width=True)
+# st.altair_chart(chart, use_container_width=True)
